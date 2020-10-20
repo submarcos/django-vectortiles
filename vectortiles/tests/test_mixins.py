506e1f4c25d0009b9cd5c1c324beeada38040ed5
J-E Castagnede <j.e.castagnede@gmail.com>
2020-10-20 15:15:32 +0200

    improve test

--- vectortiles/tests/test_mixins.py
+++ vectortiles/tests/test_mixins.py
@@ -5,5 +5,5 @@ from vectortiles.mixins import BaseVectorTileMixin
 class BaseVectorTileMixinTestCase(TestCase):
     def test_raise_not_implemented(self):
         with self.assertRaises(NotImplementedError):
-            object = BaseVectorTileMixin()
-            object.get_tile(0, 0, 0)
+            instance = BaseVectorTileMixin()
+            instance.get_tile(0, 0, 0)
