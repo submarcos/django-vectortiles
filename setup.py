63f99e23e9d8cff9de5236b8c2e9c284b5a08b68
J-E Castagnede <j.e.castagnede@gmail.com>
2020-10-20 16:32:59 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
+    python_requires='>=3.6',
     install_requires=[
         'django',
         'mercantile'
