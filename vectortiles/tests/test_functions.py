377ff9956c4bbc5aaa0b5671201450cba56cadcd
Stefan Brand <stefan.brand@eox.at>
2021-02-15 16:23:37 +0100

    ✅ Add extra brackets for Django 2.2 testing
    This hopefully fixes the failing test pipeline

--- vectortiles/tests/test_functions.py
+++ vectortiles/tests/test_functions.py
@@ -1,3 +1,4 @@
+from django import VERSION
 from django.test import TestCase
 import mercantile
 from vectortiles.postgis.functions import MakeEnvelope
@@ -7,7 +8,12 @@ from test_vectortiles.test_app.models import Feature
 
 class MakeEnvelopeTestCase(TestCase):
     def test_implicitely_transform_to_base_srid(self):
-        expected_transform = "ST_Transform(ST_MAKEENVELOPE(-20037508.342789244, -20037508.342789244, 20037508.342789244, 20037508.342789244, 3857), 4326)"
+        DJANGO_MAJOR = VERSION[0]
+        if DJANGO_MAJOR < 3:
+            # superfluous parenthesis for unknown reason
+            expected_transform = "ST_Transform((ST_MAKEENVELOPE(-20037508.342789244, -20037508.342789244, 20037508.342789244, 20037508.342789244, 3857)), 4326)"
+        else:
+            expected_transform = "ST_Transform(ST_MAKEENVELOPE(-20037508.342789244, -20037508.342789244, 20037508.342789244, 20037508.342789244, 3857), 4326)"
 
         features = Feature.objects.filter(geom__intersects=MakeEnvelope(*mercantile.xy_bounds(0, 0, 0), 3857))
 
