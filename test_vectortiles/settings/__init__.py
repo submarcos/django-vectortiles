466b888de87e7b841f7ca30ce6e10399ba70689f
J-E Castagnede <j.e.castagnede@gmail.com>
2025-07-16 16:20:30 +0200

    prepare release

--- test_vectortiles/settings/__init__.py
+++ test_vectortiles/settings/__init__.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 # Application definition
