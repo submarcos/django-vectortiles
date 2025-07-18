1e83ef63f9549e82e5b320c0f7e9664e0b9ef435
J-E Castagnede <j.e.castagnede@gmail.com>
2025-07-18 14:08:15 +0200

    manage extra domains

--- vectortiles/__init__.py
+++ vectortiles/__init__.py
@@ -1,12 +1,5 @@
-import importlib.metadata
-
 from django.utils.module_loading import import_string
 
 from vectortiles import settings as app_settings
-
-try:
-    __version__ = importlib.metadata.version(__package__ or __name__)
-except:
-    __version__ = "unknown"
 
 VectorLayer = import_string(f"{app_settings.VECTOR_TILES_BACKEND}.VectorLayer")
